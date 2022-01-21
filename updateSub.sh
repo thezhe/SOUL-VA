7fef60a2d00c44be5ad54685bb601507aae370d4
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2022-01-21 14:52:41 -0500

    updateSub

--- updateSub.sh
+++ updateSub.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # This script initializes/updates all submodules
-git submodule foreach --recursive git pull origin master
+git submodule foreach git pull origin master
+git submodule foreach git submodule update --init --recursive
+
