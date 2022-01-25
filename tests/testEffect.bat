81214d6eba3de26fa83ab659cd50cff6dfb05d83
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2022-01-25 00:04:17 -0500

    TheSplit and testEffect.bat draft

--- tests/testEffect.bat
+++ tests/testEffect.bat
@@ -1,8 +1,28 @@
 @echo off
 
-soul errors errors.soulpatch
+REM Script to run PLUG-QA on 'testEffect.soulpatch'
+REM Usage .\testEffect.bat <Fs>
+
+set FS=%1
+
+REM Check compilable
+call soul errors errors.soulpatch
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+REM PLUG-QA
+set OUT_DIR=..\tests\results
+
 cd ../PLUG-QA
-octave qaScript.m ../tests/effect.soulpatch 44100
+
+if not exist %OUT_DIR% mkdir %OUT_DIR%
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+call octave qaScript.m ..\tests\effect.soulpatch %FS% > %OUT_DIR%\qa.log
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+del ..\tests\qa.log /s /f /q > nul
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+xcopy results\* %OUT_DIR% /e /h /y /i /q > nul
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd ..\tests
