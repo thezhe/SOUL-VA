d5c0045af2e51b9b14c26df014426bb680bacc7f
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2022-01-27 14:39:47 -0500

    refactor test scripts

--- tests/runAll.bat
+++ tests/runAll.bat
@@ -4,16 +4,26 @@ REM Script to run PLUG-QA on 'testEffect.soulpatch' and soul errors on 'errors.s
 REM results and logs in 'results\'
 REM Usage .\runAll.bat <Fs>
 
-set FS="%~1"
+REM parse arguments
 
-REM Check compilable
+if "%~1"=="" goto wrongUsage
+if "%~1"=="help" goto wrongUsage
+
+if %~1 lss 44100 goto wrongFs
+if %~1 gtr 96000 goto wrongFs
+
+set FS="%~1" 
+
+REM soul errors errors.soulpatch
+
 call soul errors errors.soulpatch
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM PLUG-QA
-set OUT_DIR=..\tests\results
 
-cd ../PLUG-QA
+cd ..\PLUG-QA
+
+set OUT_DIR=..\tests\results
 
 if not exist %OUT_DIR% mkdir %OUT_DIR%
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -26,4 +36,17 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 xcopy results\* %OUT_DIR% /e /h /y /i /q > nul
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 cd ..\tests
+
+exit /b 0 
+
+REM exceptions
+
+:wrongUsage
+echo Usage: .\runAll.bat ^<^Fs^>^ 
+exit /b 1
+
+:wrongFs
+echo ^<^Fs^>^ must be in the range [44100, 96000] 
+exit /b 1
--- tests/runAll.sh
+++ tests/runAll.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-# TODO finished this script
+# NOTE: This script is not thouroughly tested. Please modify to match the behaviour of 'runAll.bat' as needed.
+
+# Script to run PLUG-QA on 'testEffect.soulpatch' and soul errors on 'errors.soulpatch'
+# results and logs in './results/'
+# Usage sudo ./runAll.sh <Fs>
 
 set -e
 
+# parse arguments
+
+FS="$1" 
+
+# soul errors errors.soulpatch
+
 soul errors errors.soulpatch
 
+# PLUG-QA
+
 cd ../PLUG-QA
-octave qaScript.m ../tests/effect.soulpatch 44100
+
+OUT_DIR=../tests/results
+
+mkdir -p $OUT_DIR
+octave qaScript.m ../tests/effect.soulpatch $FS > $OUT_DIR/qa.log
+rm -rf ../tests/qa.log
+cp -R results $OUT_DIR
+
+cd ../tests
