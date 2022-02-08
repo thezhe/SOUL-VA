c2d0f3e0f8440996a64874284ca214cbbae352ae
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2022-02-08 14:16:32 -0500

    updateSub

--- updateSub.ps1
+++ updateSub.ps1
@@ -1,10 +1,8 @@
-@echo off
-
-REM This script initializes/updates all submodules
+# This script initializes/updates all submodules
 
 git submodule foreach git pull origin master
 git submodule foreach git submodule update --init --recursive
 
-cd ./PLUG-QA
-call octave qaClear.m
+cd PLUG-QA
 
+octave qaClear.m
