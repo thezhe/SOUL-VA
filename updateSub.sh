f035222fd9d93ac39247dee88ccbafa659408f65
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2022-02-07 22:25:05 -0500

    testing changes

--- updateSub.sh
+++ updateSub.sh
@@ -4,6 +4,7 @@
 
 git submodule foreach git pull origin master
 git submodule foreach git submodule update --init --recursive
+chmod +x ./PLUG-QA/SameWav/builds/SameWav.app
 
 cd ./PLUG-QA
 octave qaClear.m
